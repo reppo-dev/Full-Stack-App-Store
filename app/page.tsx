e61a698232366b4ed086ffa9a0a64f06cf1e714a
soyr_6o <soyrozero@gmail.com>
2026-04-21 16:44:47 +0330

    adding BannerCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import BannerCarousel from "@/components/BannerCarousel";
 import Header from "@/components/Header";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <BannerCarousel />
     </div>
   );
 };
