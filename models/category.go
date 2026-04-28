0124c06d3b22ee98719b1cd4768fed6843d0062c
soyr_6o <soyrozero@gmail.com>
2026-04-28 08:51:50 +0330

    adding category controller

--- models/category.go
+++ models/category.go
@@ -4,7 +4,16 @@ import "gorm.io/gorm"
 
 type Category struct {
 	gorm.Model
-	Slug     string
-	Name     string
+	Name     string     `gorm:"type:varchar(120);not null"`
+	Slug     string     `gorm:"type:varchar(120);uniqueIndex"`
 	ParentID *uint
+	Parent   *Category
+	Children []Category `gorm:"foreignKey:ParentID"`
+}
+
+
+type CreateCategory struct {
+	Slug     string `json:"slug"`
+	Name     string `json:"name"`
+	ParentID *uint  `json:"parent_id"`
 }
